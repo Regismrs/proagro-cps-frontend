27cd4d3220f7387410258e0dc43cdb269447373e
regismrs <regismrs@hotmail.com>
2023-07-31 22:25:41 -0300

    FIX: Bug da primeira pagina quando mudava a quantidade de resultados

--- src/app/pages/comunicados-list/comunicados-paginacao/comunicados-paginacao.component.ts
+++ src/app/pages/comunicados-list/comunicados-paginacao/comunicados-paginacao.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, AfterViewInit, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
@@ -6,9 +6,10 @@ import { MatPaginator } from '@angular/material/paginator';
   templateUrl: './comunicados-paginacao.component.html',
   styleUrls: ['./comunicados-paginacao.component.scss']
 })
-export class ComunicadosPaginacaoComponent {
+export class ComunicadosPaginacaoComponent implements AfterViewInit, OnChanges{
   @ViewChild(MatPaginator, {static: true}) paginator!:MatPaginator
-  @Input() length:number = 0
+  @Input() length:number | null = null
+  @Input() pageIndex:number = 0
   @Output() pageChange:EventEmitter<{limit:number, offset:number}> = 
     new EventEmitter<{limit:number, offset:number}>()
 
@@ -16,7 +17,15 @@ export class ComunicadosPaginacaoComponent {
     this.paginator._intl.itemsPerPageLabel = 'Resultados por página';
   }
 
+  ngOnChanges(changes:SimpleChanges){
+    if (changes['length'] && this.length !== null)
+    {
+      this.paginator.pageIndex = 0
+    }
+  }
+
   onPageChange(event:any) {
+    console.info("PAGE!!")
     const offset = event.pageIndex * event.pageSize
     this.pageChange.emit({limit: event.pageSize, offset: offset})
   }
